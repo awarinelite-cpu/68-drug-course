098ee308e7411d4f3885d6f06e3b38024e103e1d
Elite <elite@wardcharts.local>
2026-09-04 07:51:31 +0000

    Add 'STAT then Q6H' / 'STAT then Q8H' frequency options (fluid loading-dose + PCM patterns)

--- functions/index.js
+++ functions/index.js
@@ -66,7 +66,8 @@ const WARDS = [
 const INTERVAL_HOURS = {
   OD: 24, BD: 12, TDS: 8, QDS: 6,
   Q4H: 4, Q6H: 6, Q8H: 8, Q12H: 12,
-  Weekly: 168
+  Weekly: 168,
+  'STAT then Q6H': 6, 'STAT then Q8H': 8
 };
 
 function toWardDate(dateStr, timeStr) {
